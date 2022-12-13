41135df76848e7b4ce10a2ce263f2d6eeef695ed
Said Saritemur <AliSaidSaritemur@gmail.com>
2022-12-14 00:39:17 +0300

    Project Started

---
+++ PokemonMArketWeb/Models/LoginUser.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PokemonsMarketWeb.Models
+{
+    public class LoginUser
+    {
+        public string mail { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ PokemonMArketWeb/Models/User.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PokemonsMarketWeb.Models
+{
+    public class User
+    {
+        [Key]
+        public int id { get; set; }
+
+        [MaxLength(35, ErrorMessage = "Username cant be so long")]
+        [MinLength(3, ErrorMessage = "Username cant be so short")]
+        [Required(ErrorMessage = "Should be username")]
+        public string userName { get; set; }
+
+
+        [Required(ErrorMessage = "Should be mail")]
+        [EmailAddress]
+        public string  mail { get; set; }
+
+        [MinLength(3, ErrorMessage = "Name cant be so short")]
+        [MaxLength(35, ErrorMessage = "Name cant be so long")]
+        [Required(ErrorMessage = "Should be name")]
+        public string  name { get; set; }
+
+        [MinLength(3, ErrorMessage = "Surname cant be so short")]
+        [MaxLength(35, ErrorMessage = "Surname cant be so long")]
+        [Required(ErrorMessage = "Should be username")]
+        public string  surname { get; set; }
+
+        [RegularExpression(@"^0?[0-9]{10}$")]
+        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "Should be phone")]
+        public string phone { get; set; }
+
+        public int  wallet{ get; set; }
+
+        [Required(ErrorMessage = "Should be password")]
+        public string password { get; set; }
+
+
+
+    }
+}

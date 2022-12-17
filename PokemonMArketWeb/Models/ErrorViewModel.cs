8f1b5121dec23ccfd221327253205ef61b766dd7
Said Saritemur <AliSaidSaritemur@gmail.com>
2022-12-17 05:50:14 +0300

    Added Some Pokemons

--- PokemonMArketWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PokemonMArketWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
